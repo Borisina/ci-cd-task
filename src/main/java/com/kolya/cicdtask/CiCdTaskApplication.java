82edc5f9faf0bced62734e50ae4d567270eb2cf7
Nikolay <bzhivo@gmail.com>
2023-12-12 22:19:27 +0500

    exclude tomcat

--- src/main/java/com/kolya/cicdtask/CiCdTaskApplication.java
+++ src/main/java/com/kolya/cicdtask/CiCdTaskApplication.java
@@ -2,10 +2,19 @@ package com.kolya.cicdtask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CiCdTaskApplication {
+public class CiCdTaskApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(CiCdTaskApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<CiCdTaskApplication> applicationClass = CiCdTaskApplication.class;
 }
